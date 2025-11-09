e31989eadc83ac6d31ab4ed3730a65b4e6ef70e2
Ahmed Hussein <92629131+ahmedhussein74@users.noreply.github.com>
2025-11-09 04:18:52 +0200

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="bg-[#ff7917] text-white text-center py-4 lg:text-xl">
-      copyright &copy; {new Date().getFullYear()} المهندس لنقل الأثاث
+      copyright &copy; {new Date().getFullYear()}  كايرو لنقل الأثاث والتغليف
       <br />
       جميع الحقوق محفوظة
     </footer>
