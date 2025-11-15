89a1e3820862ea78645afe3cfd6cf3b3f691558d
Ahmed Hussein <92629131+ahmedhussein74@users.noreply.github.com>
2025-11-15 15:53:33 +0200

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -50,7 +50,10 @@ export default function RootLayout({
         </div>
 
         <Footer />
-
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MC7TCSWK"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
       </body>
     </html>
   );
