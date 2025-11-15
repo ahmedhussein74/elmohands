dfc5534abe361639e18420979aba6e992cc497fe
Ahmed Hussein <92629131+ahmedhussein74@users.noreply.github.com>
2025-11-15 16:12:47 +0200

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,6 +36,17 @@ export default function RootLayout({
           href="delivery-truck.png"
           type="image/x-icon"
         />
+{/* Google Tag Manager */}
+        <Script id="gtm-head" strategy="beforeInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-MC7TCSWK');
+          `}
+        </Script>
+        {/* End Google Tag Manager */}
       </head>
       <body>
         {children}
@@ -50,10 +61,16 @@ export default function RootLayout({
         </div>
 
         <Footer />
-<!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MC7TCSWK"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) -->
+<{/* Google Tag Manager (noscript) */}
+<noscript>
+  <iframe
+    src="https://www.googletagmanager.com/ns.html?id=GTM-MC7TCSWK"
+    height="0"
+    width="0"
+    style={{ display: "none", visibility: "hidden" }}
+  />
+</noscript>
+{/* End Google Tag Manager (noscript) */}
       </body>
     </html>
   );
